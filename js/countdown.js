206cfda834ec54e60c8767b630129e603e803bb7
Jace Ju <jaceju@gmail.com>
2012-01-13 00:34:06 +0800

    加入時、分、秒三個 model / view

--- js/countdown.js
+++ js/countdown.js
@@ -1,30 +1,61 @@
 var Countdown = {};
 
 Countdown.Widget = Backbone.Model.extend({
-  model: null,
-  view: null,
+  hour: {
+    model: null,
+    view: null
+  },
+  minute: {
+    model: null,
+    view: null
+  },
+  second: {
+    model: null,
+    view: null
+  },
   initialize: function () {
-    this.model = new Countdown.NumModel({
+    this.hour.model = new Countdown.NumModel({
       value: 0
     });
-    this.view = new Countdown.NumView({
-      el: '.num',
-      model: this.model
+    this.hour.model.max = 12;
+    this.hour.view = new Countdown.NumView({
+      el: '.hour',
+      model: this.hour.model
+    });
+    this.minute.model = new Countdown.NumModel({
+      value: 0
+    });
+    this.minute.view = new Countdown.NumView({
+      el: '.minute',
+      model: this.minute.model
+    });
+    this.second.model = new Countdown.NumModel({
+      value: 10
+    });
+    this.second.model.bind('timeup', this.timeup, this);
+    this.second.view = new Countdown.NumView({
+      el: '.second',
+      model: this.second.model
     });
   },
   start: function () {
-    model = this.model;
+    model = this.second.model;
     model.counting = true;
     setInterval(function () {
       model.countdown();
     }, 1000);
+  },
+  timeup: function (target) {
+    if (target === this.second.model) {
+      console.log('second');
+    }
+    console.log('timeup');
   }
 });
 
 Countdown.NumModel = Backbone.Model.extend({
-  max: 19,
+  max: 59,
   counting: false,
-  timeup: false,
   default: {
     value: 0
   },
@@ -34,8 +65,8 @@ Countdown.NumModel = Backbone.Model.extend({
       if (value > 0) {
         value -= 1;
         if (0 === value) {
-          this.timeup = true;
-          this.counting = false;
+          this.counting = false;  // 停止倒數
+          this.trigger('timeup', this); // 時間到
         }
       } else {
         value = this.max;
